5370348d3fc0cdd42c3403c156d5bf0fe0ff68a7
SoftDrink <anhvu.dev.103@gmail.com>
2024-04-16 18:30:31 +0700

    config for theme, eslint, prettier, path resolver

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "simple-import-sort", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "prettier/prettier": [
+      "warn",
+      {
+        arrowParens: "always",
+        semi: true,
+        trailingComma: "none",
+        tabWidth: 2,
+        endOfLine: "auto",
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 80,
+        jsxSingleQuote: true,
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+function getAlias() {
+  const rootProject = process.cwd();
+
+  const entriesAlias = fs
+    .readdirSync(path.resolve(rootProject, 'src/'))
+    .filter(isFolder)
+    .map((folderName) => {
+      const alias = '@' + folderName;
+      const realPath = path.resolve(rootProject, 'src/' + folderName);
+      return [alias, realPath];
+    });
+
+  return Object.fromEntries(entriesAlias);
+
+  function isFolder(dirname: string): boolean {
+    return dirname.indexOf('.') === -1;
+  }
+}
+
+const foldersName = getAlias();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      plugins: [
+        [
+          '@swc/plugin-styled-components',
+          {
+            displayName: true
+          }
+        ]
+      ]
+    })
+  ],
+  resolve: {
+    alias: foldersName
+  }
+});
