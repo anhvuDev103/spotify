6782ac4f2612e20f69ebdf599b0044611789a039
SoftDrink <anhvu.dev.103@gmail.com>
2024-04-19 16:55:41 +0700

    refactor: format .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,42 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "src/**/*.css"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "simple-import-sort", "prettier"],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/**/*.css'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'simple-import-sort', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
     ],
-    "prettier/prettier": [
-      "warn",
+    'prettier/prettier': [
+      'warn',
       {
-        arrowParens: "always",
+        arrowParens: 'always',
         semi: true,
-        trailingComma: "none",
+        trailingComma: 'none',
         tabWidth: 2,
-        endOfLine: "auto",
+        endOfLine: 'auto',
         useTabs: false,
         singleQuote: true,
         printWidth: 80,
-        jsxSingleQuote: true,
-      },
+        jsxSingleQuote: true
+      }
     ],
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
     ],
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-  },
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
+  }
 };
